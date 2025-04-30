fce47528fc2a9f9f1e1ee3d2f480932b411d68b7
willjsw <sunwoo1137@gmail.com>
2025-04-30 13:27:40 +0900

    KW-226/feat:validate member duplication

--- src/main/java/com/doubleo/memberservice/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/doubleo/memberservice/domain/member/service/MemberServiceImpl.java
@@ -4,6 +4,8 @@ import com.doubleo.memberservice.domain.member.domain.Member;
 import com.doubleo.memberservice.domain.member.dto.request.MemberCreateRequest;
 import com.doubleo.memberservice.domain.member.dto.response.MemberCreateResponse;
 import com.doubleo.memberservice.domain.member.repository.MemberRepository;
+import com.doubleo.memberservice.global.exception.CommonException;
+import com.doubleo.memberservice.global.exception.errorcode.MemberErrorCode;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,8 +21,15 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public MemberCreateResponse createMember(MemberCreateRequest request) {
+        validateMember(request.email());
         String encodedPw = passwordEncoder.encode(request.password());
         Member member = memberRepository.save(Member.createMember(request.email(), encodedPw));
         return MemberCreateResponse.of(member);
+    }
+
+    private void validateMember(String email) {
+        if (memberRepository.findMemberByEmail(email).isPresent()) {
+            throw new CommonException(MemberErrorCode.MEMBER_ALREADY_EXIST);
+        }
     }
 }
