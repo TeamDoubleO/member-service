e6482e26196e304d5633036f9350e8fd3a371a7e
Jeongbeen Lee <ljb22060@gmail.com>
2025-05-30 14:56:31 +0900

    KW-537/feat: member service fcm token field 추가 (#20)

--- src/main/java/com/doubleo/memberservice/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/com/doubleo/memberservice/domain/auth/dto/request/LoginRequest.java
@@ -4,4 +4,5 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record LoginRequest(
         @Schema(description = "회원 email", example = "example@gmail.com") String email,
-        @Schema(description = "회원 패스워드", example = "pw12345") String password) {}
+        @Schema(description = "회원 패스워드", example = "pw12345") String password,
+        @Schema(description = "FCM 토큰") String fcmToken) {}
